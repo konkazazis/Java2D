490842957c068395f2e6ac6140da7992419773c1
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-10-01 16:51:03 +0200

    object interaction

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -14,6 +14,7 @@ public class Player extends Entity{
 
     public final int screenX;
     public final int screenY;
+    int hasKey = 0;
 
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
@@ -25,6 +26,8 @@ public class Player extends Entity{
         solidArea = new Rectangle();
         solidArea.x = 8;
         solidArea.y = 16;
+        solidAreaDefaultX = solidArea.x;
+        solidAreaDefaultY = solidArea.y;
         solidArea.width = 32;
         solidArea.height = 32;
 
@@ -71,6 +74,9 @@ public class Player extends Entity{
             collisionOn = false;
             gp.cCheck.checkTile(this);
 
+            int objIndex = gp.cCheck.checkObject(this, true);
+            pickUpObject(objIndex);
+
             if (collisionOn == false) {
                 switch (direction) {
                     case "up":
@@ -100,6 +106,26 @@ public class Player extends Entity{
             }
         }
 
+    }
+
+    public void pickUpObject(int i) {
+        if (i != 999) {
+            String objectName = gp.obj[i].name;
+            switch (objectName) {
+                case "Key":
+                    hasKey++;
+                    gp.obj[i] = null;
+                    System.out.println("Key: "+hasKey);
+                    break;
+                case "Door":
+                    if (hasKey > 0) {
+                        gp.obj[i] = null;
+                        hasKey--;
+                    }
+                    System.out.println("Key: "+hasKey);
+                    break;
+            }
+        }
     }
 
     public void draw(Graphics g2) {
--- src/object/OBJ_Door.java
+++ src/object/OBJ_Door.java
@@ -10,5 +10,6 @@ public class OBJ_Door extends SuperObject {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        collision = true;
     }
 }
