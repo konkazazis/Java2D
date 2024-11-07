d93ef728f38f26da028dcb56204391ec841be771
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-11-07 19:35:01 +0100

    adding lighting effect

---
+++ src/environment/Lighting.java
@@ -0,0 +1,15 @@
+package environment;
+
+import java.awt.image.BufferedImage;
+
+import main.GamePanel;
+
+public class Lighting {
+
+	GamePanel gp;
+	BufferedImage darknessFilter;
+	
+	public Lighting(GamePanel gp, int circleSize) {
+		
+	}
+}
